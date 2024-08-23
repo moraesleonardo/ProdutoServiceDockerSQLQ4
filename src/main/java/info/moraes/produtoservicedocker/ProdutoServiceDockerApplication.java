371db3598770d3de5d75f1a4db6a69db1893c76f
Leonardo <leonardobastosmoraes@hotmail.com>
2024-08-23 20:30:41 -0300

    first commit

---
+++ src/main/java/info/moraes/produtoservicedocker/ProdutoServiceDockerApplication.java
@@ -0,0 +1,13 @@
+package info.moraes.produtoservicedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoServiceDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoServiceDockerApplication.class, args);
+	}
+
+}
